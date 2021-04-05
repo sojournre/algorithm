3ac224e3d235df68cedd18066c6a61fa828b5614
Jaejin Jeong <sojournre@gmail.com>
2021-04-05 22:27:06 +0900

    Selection and Insertion sort

---
+++ chap06/SelectionSort.java
@@ -0,0 +1,13 @@
+package chap06;
+
+public class SelectionSort {
+    static void selectionSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++)
+                if (a[j] < a[min])
+                    min = j;
+            swap(a, i, min);
+        }
+    }
+}
